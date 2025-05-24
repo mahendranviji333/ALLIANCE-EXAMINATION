02ae45d467f1d272da713903283adaa148eede75
mahenranviji333 <mahendranviji333@gmail.com>
2025-05-24 22:12:45 +0530

    Branding Updated

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,41 +1,41 @@
-import React from 'react';
-import logo from './../../assets/logofont.svg';
-import './navbar.css';
+import React from "react";
+import logo from "./../../assets/au-online.png";
+import "./navbar.css";
 
 const NavLinks = () => (
-	<React.Fragment>
-		<p>
-			<a href="/">Blog</a>
-		</p>
-		<p>
-			<a href="/">Product</a>
-		</p>
-		<p>
-			<a href="/">Community</a>
-		</p>
-		<p>
-			<a href="/">Pricing</a>
-		</p>
-		<p>
-			<a href="/">Contact Us</a>
-		</p>
-		<p>
-			<a href="/login">Login</a>
-		</p>
-	</React.Fragment>
+  <React.Fragment>
+    <p className="hidden">
+      <a href="/">Blog</a>
+    </p>
+    <p className="hidden">
+      <a href="/">Product</a>
+    </p>
+    <p className="hidden">
+      <a href="/">Community</a>
+    </p>
+    <p className="hidden">
+      <a href="/">Pricing</a>
+    </p>
+    <p className="hidden">
+      <a href="/">Contact Us</a>
+    </p>
+    <p className="">
+      <a href="/login">Login</a>
+    </p>
+  </React.Fragment>
 );
 
 const Navbar = () => {
-	return (
-		<div className="landing-navbar">
-			<div className="landing-navbar-logo">
-				<img src={logo} alt="aankh-logo" />
-			</div>
-			<div className="landing-navbar-links">
-				<NavLinks />
-			</div>
-		</div>
-	);
+  return (
+    <div className="landing-navbar">
+      <div className="landing-navbar-logo">
+        <img src={logo} alt="ALLIANCE UNIVERSITY ONLINE-logo" />
+      </div>
+      <div className="landing-navbar-links">
+        <NavLinks />
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
